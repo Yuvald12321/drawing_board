527c600060983bf85b3e3119fe631145765ce757
Yuval <yuvalivshits@gmail.com>
2026-06-18 12:23:06 +0300

    bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-pyinstaller --noconsole --noconfirm --onefile --icon=logo.ico -n "drawing board" main.py
+call ".venv\Scripts\pyinstaller.exe" --specpath "build" --onefile --noconsole --icon "logo.ico" -n "drawing board" main.py
 rd /S /Q "build"
